31e287eb221a6cdc01cd6b2321a2057c67ba08bc
Charlisteketee <charlisteketee@gmail.com>
2024-02-24 21:59:11 -0800

    List Topics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
  // import PhotoListItem from './components/PhotoListItem';
  import PhotoList from './components/PhotoList';
+//  import TopicListItem from 'components/TopicListItem';
+import TopicList from 'components/TopicList';
  import './App.scss';
 
  const App = () => (
    <div className="App">
      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
      <PhotoList/>
+     <TopicList />
    </div>
  )
 
