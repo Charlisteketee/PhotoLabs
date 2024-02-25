8ad1c352a08723ab0046c45167f0da7a590b2343
Charlisteketee <charlisteketee@gmail.com>
2024-02-24 22:16:25 -0800

    Navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
- // import PhotoListItem from './components/PhotoListItem';
- import PhotoList from './components/PhotoList';
-//  import TopicListItem from 'components/TopicListItem';
+import PhotoList from './components/PhotoList';
 import TopicList from 'components/TopicList';
- import './App.scss';
+import TopNavigation from 'components/TopNavigationBar';
+import './App.scss';
 
- const App = () => (
-   <div className="App">
-     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-     <PhotoList/>
-     <TopicList />
-   </div>
- )
+const App = () => (
+  <div className="App">
+    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <TopNavigation />
+    {/* <TopicList /> */}
+    <PhotoList/>
+  </div>
+)
 
 export default App;
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -4,10 +4,10 @@ import "../styles/TopicListItem.scss";
 
 
 
-const TopicListItem = (props) => {
+const TopicListItem = ( {topic}) => {
   return (
     <div className="topic-list__item">
-      <p>{props.topic.title}</p>
+      <p>{topic.title}</p>
     </div>
   );
 };
