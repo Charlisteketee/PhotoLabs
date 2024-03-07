e2436b687e203647c0a4c1df48a8b1ad0e8525cb
Charlisteketee <charlisteketee@gmail.com>
2024-03-07 12:29:56 -0800

    Fix axios endpoints and some css styling

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -18,13 +18,14 @@ const PhotoDetailsModal = ({
 
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={closeModal}>
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
-
-      {/* Display the main photo */}
-      <img src={photo.urls.regular} alt={`Photo id: ${photo.id}`} className="photo-details-modal__image" />
-      
+      <div className='photo-details-modal__top-bar'>
+        <button className="photo-details-modal__close-button" onClick={closeModal}>
+          <img src={closeSymbol} alt="close symbol" />
+        </button>
+        {/* Display the main photo */}
+        <img src={photo.urls.regular} alt={`Photo id: ${photo.id}`} className="photo-details-modal__image" />
+      </div>
+     
       {/* Display header */}
       <div className="photo-details-modal__photographer-details">
         <img src={photo.user.profile} alt={`${photo.user.name}'s profile photo`} className="photo-list__user-profile" />
@@ -34,6 +35,8 @@ const PhotoDetailsModal = ({
 
       {/* Display similar photos */}
       <div className="photo-details-modal__images">
+      <h2 className='photo-details-modal__header'>Similar Photos</h2>
+
         <PhotoList
           photos={similarPhotos}
           toggleFavourite={toggleFavourite}
